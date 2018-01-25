da82a5edf6ddacd4ed3541269c1270e984264a57
gavin176 <17666114176@163.com>
2018-01-25 11:36:14 +0800

    Update basic_usage.py

--- basic_usage.py
+++ basic_usage.py
@@ -30,29 +30,20 @@ class PostTask(TaskSet):
     @task
     def post_method(self):
         headers = {
-            "DeviceId-Mc": "D0120D8C-09B1-45F6-AC5A-021BB6967A8E",
-            "AppId-Mc": "djigo",
-            "ClientName-Mc": "IOS-10.3.1-4.1.3",
-            "Timestamp-Mc": "1499915090820",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "zh-Hans-CN;q=1",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "User-Agent": "DJI GO 4/4.1.3 (iPhone; iOS 10.3.1; Scale/3.00)",
-            "Connection": "keep-alive",
-            "InvokeId-Mc": "16C857B7-1187-45FC-891E-CCBCDE18EED1",
-            "Sign-Mc": "BM8NJrplXWOwF/2lObV5CSe5yhI="
+            "xxx": "xxx",
+            "xxx": "xxx",
         }
         data = {
             "email":"xxx",
             "password":"xxx"
         }
         #https request need to add verify parameter
-        with self.client.post("/apis/apprest/v1/email_login",data=data,headers=headers,verify=False) as response:
+        with self.client.post("xxx",data=data,headers=headers,verify=False) as response:
             data = response.json()
             print("token is :", data["data"]["token"])
 
 class WebsiteUser(HttpLocust):
     task_set = PostTask
-    host = "https://account-api.dji.com"
+    host = "https://xxx.com"
     min_wait = 1000
     max_wait = 5000
